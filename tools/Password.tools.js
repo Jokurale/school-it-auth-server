a8a4f273ee43ba5c2debd43b9a3ce1e2612fe639
jokurale <podsiadly.michal@protonmail.com>
2021-05-18 17:14:34 +0200

    Remove unused verify function from Password.tools file

--- tools/Password.tools.js
+++ tools/Password.tools.js
@@ -4,12 +4,8 @@ const bcrypt = require("bcrypt");
 
 const salt = process.env.PASS_SALT;
 
-async function hash(password) {
-  return await bcrypt.hash(password.trim() + salt, 10);
-}
-
 async function verify(password, hash) {
   return await bcrypt.compare(password.trim() + salt, hash);
 }
 
-module.exports = { hash, verify };
+module.exports = { verify };
